a7537cd8bd77d6661f16245826d046dda9dab644
soumojit banerjee <soumojitbanerjee08@gmail.com>
2026-09-07 16:51:45 +0530

    fix get cart controller

--- src/controllers/data/get/getCartData.controller.js
+++ src/controllers/data/get/getCartData.controller.js
@@ -3,7 +3,6 @@ import prisma from "../../../lib/prisma.js";
 const getCartData = async (req, res) => {
   try {
     const clerkId = req.userId;
-
     // 1. Find User
     const user = await prisma.user.findUnique({
       where: {
@@ -37,17 +36,24 @@ const getCartData = async (req, res) => {
         data: null,
       });
     }
+
+    const responseCart = {
+      ...cart,
+      userId: cart.userId.toString(),
+    };
+    console.log("responseCart", responseCart);
+
     // 4. Return response
     return res.status(200).json({
       status: "success",
       msg: "Cart data fetched successfully",
-      data: cart,
+      data: responseCart,
     });
   } catch (error) {
+    console.error("Error fetching cart data:", error);
     res.status(500).json({
       status: "failed",
       msg: "Cart data can't be fetched",
-      error: error,
     });
   }
 };
